a55ab120d9731abb13e9110febb95c9cc6dbf03d
daihiep1988 <daihiep1988@gmail.com>
2016-03-28 21:14:20 +0700

    Update ApiConstant.java

--- app/src/main/java/constant/ApiConstant.java
+++ app/src/main/java/constant/ApiConstant.java
@@ -3,6 +3,7 @@ package constant;
 /**
  * Created by Nguyen Quang Hiep on 10/19/2015.
  */
+ // nguyen quang hiep
 public class ApiConstant {
     public static final int CONNECTION_TIME_OUT = 15;
     public static final String URL_HOST = "http://dev2.mapp.vnticketonline.vn:17000/api";
